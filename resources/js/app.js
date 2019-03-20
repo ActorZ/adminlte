11d7bdc0b420f4fba769ece5b8acbb56ffecd894
ActorZ <glumaczoran@yahoo.com>
2019-03-20 13:00:22 +0100

    video 16 end

--- resources/js/app.js
+++ resources/js/app.js
@@ -11,6 +11,7 @@ window.Vue = require('vue');
 
 import VueRouter from 'vue-router'
 import { Form, HasError, AlertError } from 'vform'
+import moment from 'moment'
 
 window.Form = Form
 Vue.component(HasError.name, HasError)
@@ -28,6 +29,14 @@ const router = new VueRouter({
   mode: 'history',
   routes // short for `routes: routes`
 })
+
+Vue.filter('upText',function(text){
+   return text.charAt(0).toUpperCase() + text.slice(1);
+});
+Vue.filter('myDate',function(datum){
+   return moment(datum).format('MMMM Do YYYY');
+});
+
 
 /**
  * The following block of code may be used to automatically register your
