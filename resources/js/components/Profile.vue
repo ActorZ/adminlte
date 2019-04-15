2058c229840f2887dafdf7c339e3fef3c2d203d8
ActorZ <glumaczoran@yahoo.com>
2019-04-15 08:27:42 +0200

    video 30 end

--- resources/js/components/Profile.vue
+++ resources/js/components/Profile.vue
@@ -267,14 +267,14 @@
                     <label for="inputName" class="col-sm-2 control-label">Name</label>
 
                     <div class="col-sm-10">
-                      <input type="email" class="form-control" id="inputName" placeholder="Name">
+                      <input v-model="form.name" type="text" class="form-control" id="inputName" placeholder="Name">
                     </div>
                   </div>
                   <div class="form-group">
                     <label for="inputEmail" class="col-sm-2 control-label">Email</label>
 
                     <div class="col-sm-10">
-                      <input type="email" class="form-control" id="inputEmail" placeholder="Email">
+                      <input v-model="form.email" type="email" class="form-control" id="inputEmail" placeholder="Email">
                     </div>
                   </div>
                   <div class="form-group">
